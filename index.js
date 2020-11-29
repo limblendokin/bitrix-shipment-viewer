eeb0f93cdca0add8a22439f822eda1da5060836f
limblendokin <limblendokin@gmail.com>
2020-11-29 15:23:07 +0300

    helmet?

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const MongoStore = require('connect-mongo')(session);
 require('dotenv').config();
 // Create the Express application
 var app = express();
-app.use(helmet());
+//app.use(helmet());
 // app.use(cors({
 //     origin: 'http://localhost:4000/login',
 //     credentials:true
